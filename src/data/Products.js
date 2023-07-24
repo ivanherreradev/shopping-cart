59d72984ec9a1a0886901f5f71661734b5578285
ivanherreradev <ivanh.developer@gmail.com>
2023-07-24 14:29:26 -0300

    add Data

---
+++ src/data/Products.js
@@ -0,0 +1,45 @@
+import burgerImg from '../assets/images/burger.png'
+import cripsImg from '../assets/images/crips.png'
+import hotdogImg from '../assets/images/hotdog.png'
+import noodlesImg from '../assets/images/noodles.png'
+import pizzaImg from '../assets/images/pizza.png'
+import tacosImg from '../assets/images/taco.png'
+
+export const ProductsData = [
+  {
+    id: 1,
+    price: 15,
+    name: 'tacos',
+    image: tacosImg
+  },
+  {
+    id: 2,
+    price: 25,
+    name: 'burger',
+    image: burgerImg
+  },
+  {
+    id: 3,
+    price: 20,
+    name: 'pizza',
+    image: pizzaImg
+  },
+  {
+    id: 4,
+    price: 35,
+    name: 'noodles',
+    image: noodlesImg
+  },
+  {
+    id: 5,
+    price: 10,
+    name: 'hot dog',
+    image: hotdogImg
+  },
+  {
+    id: 6,
+    price: 5,
+    name: 'crips',
+    image: cripsImg
+  }
+]
