75fdc6572eab480a5754e56ceaa3fd53aa99f211
ivanherreradev <ivanh.developer@gmail.com>
2023-07-24 17:06:16 -0300

    add open cart functionality and styles

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,17 @@
 import 'boxicons'
 import { useContext, useEffect, useState } from 'react'
 import { CartContext } from '../../context/CartContext'
+import ItemCart from '../ItemCart/ItemCart'
 import styles from './styles.module.scss'
 
 function Cart () {
-  const [cartOpen, setCartOpen] = useState(false)
+  const [cartOpen, setCartOpen] = useState(true)
   const [productsLength, setProductsLength] = useState(0)
   const { cartItems } = useContext(CartContext)
+
+  const handleOpenCart = () => {
+    setCartOpen(!cartOpen)
+  }
 
   useEffect(() => {
     setProductsLength(
@@ -21,7 +26,7 @@ function Cart () {
 
   return (
     <div className={styles.cart}>
-      <div className={styles.buttonCardContainer}>
+      <div onClick={handleOpenCart} className={styles.buttonCardContainer}>
         <div className={styles.buttonCard}>
           {!cartOpen
             ? (
@@ -33,13 +38,33 @@ function Cart () {
             ></box-icon>
               )
             : (
-            <box-icon name="x"></box-icon>
+            <box-icon name="x" size="md" color="#F0F0F0"></box-icon>
               )}
         </div>
         {!cartOpen && (
           <span className={styles.productsNumber}>{productsLength}</span>
         )}
       </div>
+
+      {cartItems && cartOpen && (
+        <div className={styles.openCart}>
+          <h2>Your Cart</h2>
+
+          {cartItems.length === 0
+            ? (
+            <p className={styles.emptyCart}>Your cart is empty</p>
+              )
+            : (
+            <div className={styles.productsContainer}>
+              {cartItems.map((item) => (
+                <ItemCart key={item.id} item={item} />
+              ))}
+            </div>
+              )}
+
+          <h2 className={styles.total}>Total: ${total}</h2>
+        </div>
+      )}
     </div>
   )
 }
